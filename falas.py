c0c43f5563e84d881d3beae4114a2b05ab8b88ee
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-10-14 00:28:35 -0300

    Create falas.py

---
+++ falas.py
@@ -0,0 +1,3 @@
+cumprimentar = ['Oii', 'Olá', 'Oi, tudo bem?', 'Olá, como vai?', 'oi filha da puta']
+como_esta = ['Olá eu estou bem', 'Estou otima e você', 'Estou otima, agora chupa minhas bolas', 'O mundo é uma bosta, estou viva. agora vai se fuder seu merdinha']
+humano_bem = ['Estou feliz, seu corno', 'Fico feliz merdinha', 'A legal que bom, foda-se', 'Que noticia boa! aproveita e pula da ponte']
